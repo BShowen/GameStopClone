5041e430c271a18653e68cc934c462e45b7d7f3a
Bradley Showen <BShowen@me.com>
2022-12-15 17:09:49 -0500

    Add preliminary views for Game and Console models

--- controllers/consoleController.js
+++ controllers/consoleController.js
@@ -1,10 +1,12 @@
 // get request to list all consoles
 exports.GET_consoleList = (req, res) => {
-  res.send("Respond with a CONSOLE resource");
+  res.render("consoleList");
 };
 
 // get request for a single console
 exports.GET_consoleView = (req, res) => {
   const id = req.params.consoleId;
-  res.send(`Console ID: ${id}`);
+  res.render("consoleView", {
+    id,
+  });
 };
--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,10 +1,12 @@
 // get request to list all games
 exports.GET_allGames = (req, res) => {
-  res.send("Respond with a GAME resource");
+  res.render("gameList");
 };
 
 // get request for a single game
 exports.GET_gameView = (req, res) => {
   const id = req.params.gameId;
-  res.send(`Game ID: ${id}`);
+  res.render("gameView", {
+    id,
+  });
 };
