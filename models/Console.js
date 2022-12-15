b7f0e6674adeb5793863d38c718a634b8958a3c8
Bradley Showen <BShowen@me.com>
2022-12-15 15:19:31 -0500

    Add initial models: Game, Console

---
+++ models/Console.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const consoleSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  url: String,
+  description: { type: String, required: true },
+  stock: { type: Number, required: false, default: 0 },
+  manufacturer: { type: String, required: true },
+});
+
+consoleSchema.virtual("url").get(function () {
+  return `/consoles/${this._id}`;
+});
+
+module.exports = mongoose.model("Console", consoleSchema);
---
+++ models/Game.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const gameSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  url: String,
+  genre: { type: String, required: false },
+  description: { type: String, required: true },
+  stock: { type: Number, required: false, default: 0 },
+  console: { type: Schema.Types.ObjectID, ref: "Console" },
+});
+
+gameSchema.virtual("url").get(function () {
+  return `/games/${this._id}`;
+});
+
+module.exports = mongoose.model("Game", gameSchema);
