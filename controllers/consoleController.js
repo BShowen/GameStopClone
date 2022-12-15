b07e8b6612ca4fc65cc66f054df5ef1c15874924
Bradley Showen <BShowen@me.com>
2022-12-15 18:54:27 -0500

    Update views for console

--- controllers/consoleController.js
+++ controllers/consoleController.js
@@ -1,12 +1,24 @@
+const Console = require("../models/Console");
+const mongoose = require("mongoose");
+
 // get request to list all consoles
-exports.GET_consoleList = (req, res) => {
-  res.render("consoleList");
+exports.GET_consoleList = (req, res, next) => {
+  Console.find({}).exec((err, console_list) => {
+    if (err) return next(err);
+    return res.render("consoleList", {
+      consoleList: console_list,
+    });
+  });
 };
 
 // get request for a single console
-exports.GET_consoleView = (req, res) => {
-  const id = req.params.consoleId;
-  res.render("consoleView", {
-    id,
+exports.GET_consoleView = (req, res, next) => {
+  const id = mongoose.Types.ObjectId(req.params.consoleId);
+  Console.findById(id).exec((err, console) => {
+    if (err) return next(err);
+
+    return res.render("consoleView", {
+      console,
+    });
   });
 };
