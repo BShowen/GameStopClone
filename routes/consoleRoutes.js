36db155452a258db9689a6712d06241fffbcc2ab
Bradley Showen <BShowen@me.com>
2022-12-15 21:04:17 -0500

    Implement update for Console model

--- routes/consoleRoutes.js
+++ routes/consoleRoutes.js
@@ -6,6 +6,14 @@ const consoleController = require("../controllers/consoleController");
 // Handle GET request to display all consoles
 router.get("/", consoleController.GET_consoleList);
 
+// Handle GET request to update a console
+router.get("/:consoleId/update", consoleController.GET_consoleUpdate);
+
+// Handle POST request to update a console
+router.post("/:consoleId/update", consoleController.POST_consoleUpdate);
+
+router.get("/:consoleId/delete", consoleController.GET_consoleDelete);
+
 // Handle GET request to view a single console
 router.get("/:consoleId", consoleController.GET_consoleView);
 
