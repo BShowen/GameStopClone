6879f5108c680fab33425b23ddc94dce3edd80bd
Bradley Showen <BShowen@me.com>
2023-01-01 14:47:24 -0500

    Implement image upload/edit/delete for accessory model

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -2,6 +2,8 @@ const Accessory = require("../models/Accessory");
 const GameConsole = require("../models/GameConsole");
 const mongoose = require("mongoose");
 const async = require("async");
+const upload = require("./helpers/multerUpload");
+const deleteImage = require("./helpers/deleteImage");
 
 // GET request for all accessories
 exports.GET_allAccessories = (req, res, next) => {
@@ -28,14 +30,19 @@ exports.GET_accessoryCreate = (req, res, next) => {
 };
 
 // POST request to create a new accessory
-exports.POST_accessoryCreate = (req, res, next) => {
-  const newAccessory = new Accessory({ ...req.body });
-  newAccessory.save((err, savedAccessory) => {
-    if (err) return next(err);
-
-    res.redirect(savedAccessory.url);
-  });
-};
+exports.POST_accessoryCreate = [
+  upload.single("model-image"),
+  (req, res, next) => {
+    const newAccessory = new Accessory({
+      ...req.body,
+      img_path: req.file ? `/uploads/${req.file.filename}` : undefined,
+    });
+    newAccessory.save((err, savedAccessory) => {
+      if (err) return next(err);
+      res.redirect(savedAccessory.url);
+    });
+  },
+];
 
 // GET request to update a new accessory
 exports.GET_accessoryUpdate = (req, res, next) => {
@@ -73,22 +80,82 @@ exports.GET_accessoryUpdate = (req, res, next) => {
 };
 
 // POST request to update a new accessory
-exports.POST_accessoryUpdate = (req, res, next) => {
-  // get the accessory from the DB and update it
-  const id = mongoose.Types.ObjectId(req.params.id);
-  const oldAccessory = new Accessory({ ...req.body, _id: id });
-  Accessory.findByIdAndUpdate(id, oldAccessory).exec((err, newAccessory) => {
-    if (err) return next(err);
+exports.POST_accessoryUpdate = [
+  upload.single("model-image"),
+  (req, _, next) => {
+    // Find the accessory in the DB.
+    const accessoryId = mongoose.Types.ObjectId(req.params.id);
+    Accessory.findById(accessoryId).exec((err, accessory) => {
+      if (err) return next(err);
+      req.accessory = accessory;
+      next();
+    });
+  },
+  // This function removes the model's image when the user has removed the image
+  (req, _, next) => {
+    // If the user wants to delete the image for this model.
+    if (req.body["delete-photo"]) {
+      // Remove the form attr. that tells this method to delete the image.
+      // If not removed then there will be an unexpected attribute error
+      // from mongoose.
+      delete req.body["delete-photo"];
 
-    res.redirect(newAccessory.url);
-  });
-};
+      // Delete the image or log the error if image isn't found
+      deleteImage({ model: req.accessory }, (err) => {
+        // An error deleting the photo is simply logged. Ideally this would be
+        // sent to some logger or ticketed for the dev to review. Because this
+        // is not a real world app I am simply logging the error.
+        console.log(err);
+      });
+
+      // model.img_path must be set to this string, otherwise model.hasImage will
+      // return true when it should return false. What is happening is an
+      // attribute it being changed on the model, but the hasImage method is
+      // operating on the value before it was changed.
+      req.accessory.img_path = "/images/placeholder_image.jpg";
+    }
+    next();
+  },
+  (req, res, next) => {
+    const { accessory } = req;
+    // Create a new accessory model with params from the form.
+    const updatedAccessory = new Accessory({
+      ...req.body,
+      _id: accessory._id,
+      img_path: accessory.img_path,
+    });
+
+    if (req.file) {
+      // If the user is supplying an image.
+      updatedAccessory.img_path = `/uploads/${req.file.filename}`;
+    }
+
+    // Perform the update.
+    Accessory.findByIdAndUpdate(accessory._id, updatedAccessory).exec(
+      (err, newAccessory) => {
+        if (err) return next(err);
+
+        res.redirect(newAccessory.url);
+      }
+    );
+  },
+];
 
 // GET request to delete a new accessory
 exports.GET_accessoryDelete = (req, res, next) => {
   const id = mongoose.Types.ObjectId(req.params.id);
-  Accessory.findByIdAndDelete(id).exec((err) => {
+  Accessory.findByIdAndDelete(id).exec((err, accessory) => {
     if (err) return next(err);
+
+    // Delete the image associated with this model.
+    if (accessory.hasImage) {
+      deleteImage({ model: accessory }, (err) => {
+        // An error deleting the photo is simply logged. Ideally this would be
+        // sent to some logger or ticketed for the dev to review. Because this
+        // is not a real world app I am simply logging the error.
+        console.log(err);
+      });
+    }
 
     res.redirect("/accessories");
   });
--- models/Accessory.js
+++ models/Accessory.js
@@ -7,10 +7,19 @@ const accessorySchema = new Schema({
   price: { type: Number, required: true },
   description: { type: String, required: true },
   stock: { type: Number, required: true },
+  img_path: {
+    type: String,
+    default: "/images/placeholder_image.jpg",
+  },
 });
 
 accessorySchema.virtual("url").get(function () {
   return `/accessories/${this._id}`;
+});
+
+accessorySchema.virtual("hasImage").get(function () {
+  // Return true if this model has a user uploaded image.
+  return this.img_path != "/images/placeholder_image.jpg";
 });
 
 module.exports = mongoose.model("Accessory", accessorySchema);
